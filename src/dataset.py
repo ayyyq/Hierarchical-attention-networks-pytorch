fef39de81a7b4eedaaf777f6e2b1b214ab471788
Lexi <yyqcode@gmail.com>
2020-11-17 19:26:53 +0800

    data process

--- src/dataset.py
+++ src/dataset.py
@@ -1,69 +1,94 @@
 """
 @author: Viet Nguyen <nhviet1009@gmail.com>
 """
-import pandas as pd
+import torch
 from torch.utils.data.dataset import Dataset
-import csv
-from nltk.tokenize import sent_tokenize, word_tokenize
-import numpy as np
+from transformers import BertTokenizer
+import xml.etree.ElementTree as ET
 
 
 class MyDataset(Dataset):
 
-    def __init__(self, data_path, dict_path, max_length_sentences=30, max_length_word=35):
+    def __init__(self, data_path, dict_path, max_seq_num=40):
         super(MyDataset, self).__init__()
+        self.texts, self.labels, self.num_classes, self.max_seq_num, self.max_seq_len = self.process_xml(data_path,
+                                                                                                    dict_path)
+        print("num_classes", self.num_classes)
+        print("max_seq_num", self.max_seq_num)
+        print("max_seq_len", self.max_seq_len)
 
-        texts, labels = [], []
-        with open(data_path) as csv_file:
-            reader = csv.reader(csv_file, quotechar='"')
-            for idx, line in enumerate(reader):
-                text = ""
-                for tx in line[1:]:
-                    text += tx.lower()
-                    text += " "
-                label = int(line[0]) - 1
-                texts.append(text)
-                labels.append(label)
+        if self.max_seq_num > max_seq_num:
+            self.max_seq_num = max_seq_num
 
-        self.texts = texts
-        self.labels = labels
-        self.dict = pd.read_csv(filepath_or_buffer=dict_path, header=None, sep=" ", quoting=csv.QUOTE_NONE,
-                                usecols=[0]).values
-        self.dict = [word[0] for word in self.dict]
-        self.max_length_sentences = max_length_sentences
-        self.max_length_word = max_length_word
-        self.num_classes = len(set(self.labels))
+    def process_xml(self, data_path, dict_path):
+        # 解析xml文件，并tokenize
+        tok = BertTokenizer.from_pretrained(dict_path)
+        max_seq_num = 0
+        max_seq_len = 0
+
+        docs = []  # [doc_num, seq_len], list of list
+        texts = []  # [doc_num, seq_num, seq_len], list of list of list
+        labels = []  # [doc_num], list
+        label2idx = {}
+
+        tree = ET.parse(data_path)
+        root = tree.getroot()
+        for document_set in root:
+            for document in document_set:
+                # label
+                label = document.attrib['document_level_value']
+                if label not in label2idx:
+                    label2idx[label] = len(label2idx)
+                labels.append(label2idx[label])
+
+                # text
+                for sentence in document:
+                    if sentence.text == '-EOP- .':
+                        continue
+                    sent = ''
+                    for text in sentence.itertext():
+                        sent += text
+                    sent = sent.replace('-EOP- ', '').lower()
+
+                    # tokenize
+                    tok_sent = tok.encode(sent)
+                    if len(tok_sent) > max_seq_len:
+                        max_seq_len = len(tok_sent)
+                    docs.append(tok_sent)
+
+                if len(docs) > max_seq_num:
+                    max_seq_num = len(docs)
+                texts.append(docs)
+                docs = []
+        return texts, labels, len(label2idx), max_seq_num, max_seq_len
+
+    def get_label_size(self):
+        return
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, index):
-        label = self.labels[index]
-        text = self.texts[index]
-        document_encode = [
-            [self.dict.index(word) if word in self.dict else -1 for word in word_tokenize(text=sentences)] for sentences
-            in
-            sent_tokenize(text=text)]
+        label = self.labels[index]  # scalar
+        doc = self.texts[index]  # doc, list of list
+        masks = []
 
-        for sentences in document_encode:
-            if len(sentences) < self.max_length_word:
-                extended_words = [-1 for _ in range(self.max_length_word - len(sentences))]
-                sentences.extend(extended_words)
+        for sent in doc:
+            mask = [1] * len(sent) + [0] * (self.max_seq_len - len(sent))
+            masks.append(mask)
+            if len(sent) < self.max_seq_len:
+                sent += [0] * (self.max_seq_len - len(sent))
+        if len(doc) < self.max_seq_num:
+            masks += [[0] * self.max_seq_len] * (self.max_seq_num - len(doc))
+            doc += [[0] * self.max_seq_len] * (self.max_seq_num - len(doc))
+        elif len(doc) > self.max_seq_num:
+            masks = masks[:self.max_seq_num]
+            doc = doc[:self.max_seq_num]
 
-        if len(document_encode) < self.max_length_sentences:
-            extended_sentences = [[-1 for _ in range(self.max_length_word)] for _ in
-                                  range(self.max_length_sentences - len(document_encode))]
-            document_encode.extend(extended_sentences)
-
-        document_encode = [sentences[:self.max_length_word] for sentences in document_encode][
-                          :self.max_length_sentences]
-
-        document_encode = np.stack(arrays=document_encode, axis=0)
-        document_encode += 1
-
-        return document_encode.astype(np.int64), label
+        return torch.tensor(doc), torch.tensor(masks), torch.tensor(label, dtype=torch.long)
 
 
 if __name__ == '__main__':
-    test = MyDataset(data_path="../data/test.csv", dict_path="../data/glove.6B.50d.txt")
-    print (test.__getitem__(index=1)[0].shape)
+    test = MyDataset(data_path="../data/dlef_corpus/english.xml", dict_path="../data/bert-base-uncased")
+    doc, masks, label = test.__getitem__(index=701)
+    print("end")
